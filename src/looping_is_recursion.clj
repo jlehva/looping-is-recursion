868a0c04dc90a5e3eabe4688d4ef4837eb3fc15a
jykke <jyri.lehva@saunalahti.fi>
2015-02-15 15:24:30 +0200

    exercise 3 seq=

--- src/looping_is_recursion.clj
+++ src/looping_is_recursion.clj
@@ -15,7 +15,12 @@
     (helper nil a-seq)))
 
 (defn seq= [seq1 seq2]
-  ":(")
+  (cond
+    (and (empty? seq1) (empty? seq2)) true
+    (or (empty? seq1) (empty? seq2)) false
+    (not (= (first seq1) (first seq2))) false
+    :else
+      (recur (rest seq1) (rest seq2))))
 
 (defn find-first-index [pred a-seq]
   ":(")
